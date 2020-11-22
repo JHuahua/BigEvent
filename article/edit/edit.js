845968bfe11dca469b14dbbc0d7848ef7c6861d9
huahua <2023818989@qq.com>
2020-11-22 18:59:08 +0800

    修改文章功能完成

--- article/edit/edit.js
+++ article/edit/edit.js
@@ -1,3 +1,7 @@
+// -------------------------------------------------------获取文章id
+var str = window.location.search;
+var id = str.slice(4);
+
 // -------------------------------------------------------加载分类数据
 var form = layui.form;
 $.ajax({
@@ -10,6 +14,8 @@ $.ajax({
         $("select").html(str);
         // 重新渲染form
         form.render("select")
+
+        getList()
     }
 })
 
@@ -20,7 +26,7 @@ function initCropper() {
     // - 调用cropper方法，创建剪裁区
     $('#image').cropper({
         // 纵横比(宽高比)
-        aspectRatio: 400 / 280, 
+        aspectRatio: 400 / 280, // 正方形
         // 指定预览区域
         preview: '.img-preview' // 指定预览区的类名（选择器）
     });
@@ -45,58 +51,62 @@ function initCropper() {
     })
 }
 
-// ------------------------------------------------发布
-$("form").on("submit", function(e) {
+// ------------------------------------------------获取文章详情 展示
+function getList() {
+    $.ajax({
+        url: '/my/article/' + id,
+        success: function (res) {
+            // console.log(res);
+            form.val("edit", res.data)
+        }
+    })
+}
+
+
+// ------------------------------------------------确认修改
+$("form").on("submit", function (e) {
+    var index = layer.load(2, { shade: [0.8, '#393D49'] });
+
     e.preventDefault();
-  
-    // 提交数据 formData(); ajax配置！后台接口支持！必须有name属性配置；
+
     var fd = new FormData(this);
-    // console.log(fd);
-  
-    // 遍历：可以看到内部已经添加什么数据？
-    // fd.forEach(function (val, key) {
-    //   console.log(val, key);
-    // })
-  
-    // 
-    // 原因：这个两个区域，已经被各自插件所加载了！
-    // 解决：想办法把img 富文本内容 添加到fd；
-  
-    //   1.富文本有名，没有值；需要重新设置下！查文档找如何获取内容！
-    fd.set("content", tinyMCE.activeEditor.getContent());
-    //   2.添加 裁剪后的图片信息   cover_img：值 文件对象；
-    //     借助 canvas 查文档 获取裁剪后的文件对象！
+
+    // 替换FormData对象里面的一项
+    fd.set('content', tinyMCE.activeEditor.getContent());
+    fd.append('Id', id);
+
+    // 剪裁图片
     var canvas = $('#image').cropper('getCroppedCanvas', {
-      width: 400,
-      height: 280
+        width: 400,
+        height: 280
     });
+    // 
     //    canvas 用户获取文件对象,不是前面用户 base64字符串
     canvas.toBlob(function(file) { // 文件对象
-      // 添加img 参数值：
-      fd.append("cover_img", file);
-  
-      // 
-      // 完成添加
-      $.ajax({
-        type: 'POST',
-        url: '/my/article/add',
-        // fd:对象;
-        data: fd,
-        // 提交formdata数据，必须加下面两个选项
-        processData: false,
-        contentType: false,
-        success: function(res) {
-          layer.msg(res.message);
-          // 设计：
-          if (res.status == 0) {
-            // 页面转跳到 文章列表 页面
-            location.href = '/article/list/list.html';
-  
-            // 响应dom节点 类名添加
-            var dom = window.parent.document.querySelector("#wzlist");
-            $(dom).addClass("layui-this").next().removeClass("layui-this");
+        // 添加img 参数值：
+        fd.append("cover_img", file);
+    
+        // 
+        // 完成添加
+        $.ajax({
+          type: 'POST',
+          url: '/my/article/edit',
+          // fd:对象;
+          data: fd,
+          // 提交formdata数据，必须加下面两个选项
+          processData: false,
+          contentType: false,
+          success: function(res) {
+            layer.msg(res.message);
+            // 设计：
+            if (res.status == 0) {
+                // 页面转跳到 文章列表 页面
+                location.href = '/article/list/list.html';
+    
+                // 关闭层
+                layer.close(index)
+            }
           }
-        }
-      });
+        });
     }); 
-});
+})
